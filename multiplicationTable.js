76b9aa03fb11567c91b5d181fab0f18284c995d5
Teodor Petrov <teodor.petrov@abv.bg>
2023-02-16 10:24:57 +0200

    new files

---
+++ multiplicationTable.js
@@ -0,0 +1,10 @@
+function multiplicationTable(params) {
+    for (let n1 = 1; n1 < 11; n1++) {
+        for (let n2 = 1; n2 < 11; n2++) {
+            console.log(`${n1} * ${n2} = ${n1*n2}`);
+        }
+        
+    }
+}
+
+multiplicationTable()
